62c94f3c3097f47116d0643806254ff46664c64c
Gabriela1030 <gabriela518119@gmail.com>
2024-10-10 23:05:58 -0500

    Taler 4

---
+++ Taller cuatro corregido/Taller-cuatro/src/App.jsx
@@ -0,0 +1,15 @@
+// componente de tarjeta de consejo y el archivo de estilos
+import React from 'react';
+import { AdviceCard } from './components/AdviceCard';
+import './styles/index.css';
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <AdviceCard />
+    </div>
+  );
+}
+
+export default App;
